a55913dca2042307ac41d55a4e5d3252ab74793e
Christopher Kwon <christopherkwon@ChristophersMBP.fios-router.home>
2023-05-01 14:45:11 -0400

    Added new home page sections

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Nav } from './components/Nav';
 import { Hero } from './components/Hero';
 import { Specials } from './components/Specials';
-// import { Testimonials } from './components/Testimonials';
+import { Testimonials } from './components/Testimonials';
 // import { About } from './components/About';
 // import { Footer } from './components/Footer';
 
@@ -12,8 +12,8 @@ export default function App() {
         <Nav />
         <Hero />
         <Specials />
-        {/* <Testimonials />
-        <About />
+        <Testimonials />
+        {/*<About />
         <Footer /> */}
     </>
   );
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,27 +1,57 @@
 import React from 'react';
 import './Testimonials.css';
+import { FaStar } from "react-icons/fa";
 
 export function Testimonials() {
     return (
         <div className="Testimonials">
-            <section className="TestiMain">
-                <h2>Testimonials</h2>
-                <p>Hear from some of our satisfied customers!</p>
-            </section>
-            <article className="TestiReviews">
-                <card>
-                    <h4>Bob Smith</h4>
-                    <p>Yorem ipsum dolor sit amet, consectetur adipiscing elit. Yorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </card>
-                <card>
-                    <h4>Nancy Jones</h4>
-                    <p>Yorem ipsum dolor sit amet, consectetur adipiscing elit. Yorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </card>
-                <card>
-                    <h4>Joe Guy</h4>
-                    <p>Yorem ipsum dolor sit amet, consectetur adipiscing elit. Yorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </card>
-            </article>
+            <div className="TestiSection">
+                <section className="TestiMain">
+                    <h2>Testimonials</h2>
+                    <p>Hear from some of our satisfied customers!</p>
+                </section>
+                <article className="TestiReviews">
+                    <card>
+                        <div className="TopRow">
+                            <h4>Bob Smith</h4>
+                        </div>
+                        <div>
+                            <FaStar className="checked" />
+                            <FaStar className="checked" />
+                            <FaStar className="checked" />
+                            <FaStar className="checked" />
+                            <FaStar className="checked" />
+                        </div>
+                        <p>Yorem ipsum dolor sit amet, consectetur adipiscing elit. Yorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </card>
+                    <card>
+                        <div className="TopRow">
+                            <h4>Nancy Jones</h4>
+                        </div>
+                        <div>
+                            <FaStar className="checked" />
+                            <FaStar className="checked" />
+                            <FaStar className="checked" />
+                            <FaStar className="checked" />
+                            <FaStar className="unchecked" />
+                        </div>
+                        <p>Yorem ipsum dolor sit amet, consectetur adipiscing elit. Yorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </card>
+                    <card>
+                        <div className="TopRow">
+                            <h4>Joe Guy</h4>
+                        </div>
+                        <div>
+                            <FaStar className="checked" />
+                            <FaStar className="checked" />
+                            <FaStar className="checked" />
+                            <FaStar className="checked" />
+                            <FaStar className="checked" />
+                        </div>
+                        <p>Yorem ipsum dolor sit amet, consectetur adipiscing elit. Yorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </card>
+                </article>
+            </div>
         </div>
     );
 }
